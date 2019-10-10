88eb3ec3b0b845e3ac0e832fe9901d6266a09a87
Triptixx <54292597+triptixx@users.noreply.github.com>
2019-10-10 23:23:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
 set -eo pipefail
 
+# ANSI colour escape sequences
+RED='\033[0;31m'
+CYANBOLD='\033[1;36m'
+RESET='\033[0m'
+
+CONFIG_DIR='/rundir;/storage;/config'
+
+for FOLD in `echo $FOLDS | tr ';' '\n'`; do
+    if su-exec -e [ ! -w "$CONFIG_DIR" ]; then
+        2>&1 echo -e "${RED}####################### WARNING #######################${RESET}"
+        2>&1 echo
+        2>&1 echo -e "${RED}     No permission to write in '$CONFIG_DIR' directory.${RESET}"
+        2>&1 echo -e "${RED}       Correcting permissions to prevent a crash.${RESET}"
+        2>&1 echo
+        2>&1 echo -e "${RED}#######################################################${RESET}"
+        2>&1 echo
+
+        chown $SUID:$SGID "$CONFIG_DIR"
+    fi
+done
